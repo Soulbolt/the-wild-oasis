2f39c1cf116e95b35811b2ab3593035b1b629507
Soulbolt <soulbolt@gmail.com>
2024-02-24 17:07:51 -0700

    Add new props with h4 in Heading component. Add missing FormRowVertical component. Add apiAuth component with function login passing props email and password, async await supabase sign in address. Conditional for error. Add Logo, Heading, Loginform components to Login component. Add useLogin component, useNavigation variable, useMutation variables to pass on email and password to the loginApi, navigate to dashboard on success, display toast message on error. Add useState varaibles for email, password. Add useLogin variables. implment logic to prevent default, conditonal if credentials are empty, pass email and password to login function. disable inputs and button if isLoading. Add spinner mini to button if isLoading.

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -28,6 +28,16 @@ ${(props) =>
     `}
 
   line-height: 1.4;
+
+  ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+      text-align: center;
+    `}
+
+  line-height: 1.4;
 `;
 
 export default Heading;
