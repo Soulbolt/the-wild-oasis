33fc6de8b041e7c2e56a40c7bbc206015e457a21
Soulbolt <soulbolt@gmail.com>
2024-02-26 13:09:10 -0700

    Remove hardcoded credentials.

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("cesar@gmail.com");
-  const [password, setPassword] = useState("password[manga]");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
